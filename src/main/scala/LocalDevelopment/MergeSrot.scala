91bdce955c76819693f4ab243dc957fff0bcc73d
Chuan <kirk336neo@gmail.com>
2024-09-08 18:10:20 +0800

    Finish generic merge sort

--- src/main/scala/LocalDevelopment/MergeSrot.scala
+++ src/main/scala/LocalDevelopment/MergeSrot.scala
@@ -1,19 +1,19 @@
 package LocalDevelopment
 
 object MergeSrot extends App {
-  def mergeSrot(items: Array[Int]): Array[Int] = {
+  def mergeSrot[T: Ordering](items: IndexedSeq[T]): IndexedSeq[T] = {
     if (items.length <= 1) items
     else {
       val (left, right) = items.splitAt(n = items.length / 2)
       val (sortedLeft, sortedRight) = (mergeSrot(left), mergeSrot(right))
       var (leftIdx, rightIdx) = (0, 0)
-      val output = Array.newBuilder[Int]
+      val output = IndexedSeq.newBuilder[T]
       
       while (leftIdx < sortedLeft.length || rightIdx < sortedRight.length) {
         val takeLeft = (leftIdx < sortedLeft.length, rightIdx < sortedRight.length) match {
           case (true, false) => true
           case (false, true) => false
-          case (true, true) => sortedLeft(leftIdx) < sortedRight(rightIdx)
+          case (true, true) => Ordering[T].lt(sortedLeft(leftIdx), sortedRight(rightIdx))
         }
         if (takeLeft) {
           output += sortedLeft(leftIdx)
@@ -28,4 +28,5 @@ object MergeSrot extends App {
   }
   
   println(mergeSrot(Array(77, 2, 654, 99, 0 , 28, 43, 1023, 37)).mkString(", "))
+  println(mergeSrot(Vector("banana", "apple", "durian", "cabbage")).mkString(", "))
 }
